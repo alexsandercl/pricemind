59bf929fce78075a0c074884d54f36022ba94179
alexsandercl <alexsander9195@gmail.com>
2026-02-14 21:44:05 -0300

    Fix all Case Sensitive (Linux Deploy)

--- backend/src/routes/SupportRoutes.js
+++ backend/src/routes/SupportRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const supportController = require("../controllers/supportController");
+const supportController = require("../controllers/SupportController");
 const authMiddleware = require("../middlewares/authMiddleware");
 
 // ============================================
